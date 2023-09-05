cf29de58068b1a9f63a3d8f19d21940aff0dffed
Strider <mezentsev.k@gmail.com>
2023-09-05 22:58:04 +0800

    Создана первая книга

---
+++ src/Book.java
@@ -0,0 +1,17 @@
+public class Book {
+    private String title;
+    private Author author;
+    private int year;
+
+    public Book(String title, Author author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+    public String getTitle() {
+        return this.title;
+    }
+    public int getYear() {
+        return this.year;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Объекты и Классы");
+        Author levTolstoy = new Author("Лев", "Толстой");
+        Book sunday = new Book("Воскресенье", levTolstoy, 1899);
+        System.out.println("Название книги: " + sunday.getTitle());
+        System.out.println("Автор: " + levTolstoy.getName() + levTolstoy.getSurName());
+        System.out.println("Год издания: " + sunday.getYear());
     }
+
 }
